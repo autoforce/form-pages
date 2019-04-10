df6c4f80781c76dcbd9fc960ed462384c8e1c300
Júnior Garcia <itamarjr91@gmail.com>
2019-04-10 13:34:46 -0300

    Added customScripts folder to the docs.

---
+++ docs/customScripts/addAnalytics.js
@@ -0,0 +1,11 @@
+(function() {
+    var script = document.createElement('script');
+    script.src = "https://www.googletagmanager.com/gtag/js?id=UA-138089850-2";
+    script.async = true;
+    document.body.appendChild(script);
+})();
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-138089850-2');
